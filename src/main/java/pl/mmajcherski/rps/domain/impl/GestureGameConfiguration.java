ec3e20f777717bf6c1f9042157942620a78503e6
Michał Majcherski <mmajcherski@gmail.com>
2013-08-03 11:36:01 +0200

    Fixing game configuration immutability.

--- src/main/java/pl/mmajcherski/rps/domain/impl/GestureGameConfiguration.java
+++ src/main/java/pl/mmajcherski/rps/domain/impl/GestureGameConfiguration.java
@@ -1,6 +1,7 @@
 package pl.mmajcherski.rps.domain.impl;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -118,7 +119,7 @@ public class GestureGameConfiguration {
 	}
 	
 	public List<GameEventsListener> getGameEventsListeners() {
-		return gameEventsListeners;
+		return Collections.unmodifiableList(gameEventsListeners);
 	}
 	
 }
